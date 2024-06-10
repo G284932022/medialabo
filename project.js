148a9b2ccb6f36f164fdaf21f6f0c90a7246e7b9
G284932022 <228493@st.takushoku-u.ac.jp>
2024-06-10 23:03:17 +0900

    更新

--- project.js
+++ project.js
@@ -1,40 +1,63 @@
-let b = document.querySelector('#tosi');
-b.addEventListener('click', tosi);
+const searchButton = document.getElementById('searchButton');
+searchButton.addEventListener('click', searchWeather);
 
-function sendRequest() {
-    // URL を設定
-    let url = 'https://www.nishita-lab.org/web-contents/jsons/'+ key + '.json';
+function searchWeather() {
+  const searchInput = document.getElementById('search').value;
+  const cityId = getCityId(searchInput);
 
-    // 通信開始
-    axios.get(url)
-        .then(showResult)   // 通信成功
-        .catch(showError)   // 通信失敗
-        .then(finish);      // 通信の最後の処理
+  if (cityId) {
+    const apiUrl = `https://www.nishita-lab.org/web-contents/jsons/openweather/${cityId}.json`;
+    fetch(apiUrl)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('都市の天気情報が見つかりませんでした。');
+        }
+        return response.json();
+      })
+      .then(data => displayWeather(data))
+      .catch(error => {
+        const resultDiv = document.getElementById('result');
+        resultDiv.innerHTML = `<p>${error.message}</p>`;
+      });
+  } else {
+    console.log('都市名が正しくありません。');
+  }
 }
 
-// 通信が成功した時の処理
-function showResult(resp) {
-    // サーバから送られてきたデータを出力
-    let data = resp.data;
+function getCityId(cityName) {
+  const cityIds = {
+    'カイロ': 360630,
+    'モスクワ': 524901,
+    'ヨハネスブルク': 993800,
+    '北京': 1816670,
+    '東京': 1850147,
+    'シンガポール': 1880252,
+    'シドニー': 2147714,
+    'ロンドン': 2643743,
+    'パリ': 2968815,
+    'リオデジャネイロ': 3451189,
+    'ニューヨーク': 5128581,
+    'ロサンゼルス': 5368361
+  };
 
-    // data が文字列型なら，オブジェクトに変換する
-    if (typeof data === 'string') {
-        data = JSON.parse(data);
-    }
+  const cityNamesInKatakana = Object.keys(cityIds);
+  const cityNameInKatakana = cityNamesInKatakana.find(
+    name => name === cityName
+  );
 
-    // data をコンソールに出力
-    console.log(data);
-
-    // data.x を出力
-    console.log(data.x);
+  return cityNameInKatakana ? cityIds[cityNameInKatakana] : null;
 }
 
-// 通信エラーが発生した時の処理
-function showError(err) {
-    console.log(err);
-}
-
-// 通信の最後にいつも実行する処理
-function finish() {
-    console.log('Ajax 通信が終わりました');
+function displayWeather(data) {
+  const resultDiv = document.getElementById('result');
+  resultDiv.innerHTML = `
+    <h2>${data.name}</h2>
+    <p>天気: ${data.weather[0].description}</p>
+    <p>気温: ${data.main.temp} ℃</p>
+    <p>最低気温: ${data.main.temp_min} ℃</p>
+    <p>最高気温: ${data.main.temp_max} ℃</p>
+    <p>湿度: ${data.main.humidity} %</p>
+    <p>風速: ${data.wind.speed} m/s</p>
+    <p>風向: ${data.wind.deg} °</p>
+  `;
 }
